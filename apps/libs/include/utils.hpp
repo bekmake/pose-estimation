88357f1e047ff33741283e2ac7d0a7638cab5c42
bekmake <bekmake@gmail.com>
2022-08-31 12:26:19 +0200

    preparing functions for class

--- apps/libs/include/utils.hpp
+++ apps/libs/include/utils.hpp
@@ -5,7 +5,7 @@ using namespace cv;
 using namespace std;
 
 void iterateThroughFolder(String folderPath, vector<String> &imagePaths);
-void loadDetectAprilTag(String ImagePath, Mat &image, Ptr<aruco::Dictionary> &dictionary, vector<vector<Point2f>> &corners, vector<int> &ids);
+void detectAprilTag(Mat &image, vector<vector<Point2f>> &corners, vector<int> &ids);
 void crossProduct(Mat &a, Mat &b, Mat &c);
 void drawCube(Mat &image, Mat &pt1Cube, Mat &pt2Cube, Mat &pt3Cube, Mat &pt4Cube);
 void horizontallyConcatThreeMat(Mat &A, Mat &B, Mat &C, Mat &D, Mat &E);
